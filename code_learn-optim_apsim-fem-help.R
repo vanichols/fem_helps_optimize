04dcd9b5951fabea897ca41886a2e55cb466a9c5
vanichols <vnichols@iastate.edu>
2020-10-19 11:56:24 -0500

    add script with param prob

--- code_learn-optim_apsim-fem-help.R
+++ code_learn-optim_apsim-fem-help.R
@@ -1,4 +1,4 @@
-# goal: learn to use optim_apsim
+# goal: get help from FEM getting path to coldTT parameter
 #
 # created: 10/10/20
 #
@@ -14,54 +14,18 @@ rm(list = ls())
 #remotes::install_github("femiguez/apsimx")
 library(apsimx)
 library(dplyr)
-library(readr)
-library(ggplot2)
 library(xml2)
-library(tidysawyer2)
-library(lubridate)
-library(saapsim)
-
-curdir <- paste(getwd())
-curdir
-
-#--tell it the correct apsim
-apsim_options(exe.path = "C:/PROGRA~2/Apsim79-r0/Model/Apsim.exe")
-
-
-#--example of how to get parameter path from vignette
-extd.dir <- system.file("extdata", package = "apsimx")
-rue.pth <- inspect_apsim_xml("Maize75.xml", src.dir = extd.dir, parm = "rue")
-ext.pth <- inspect_apsim_xml("Maize75.xml", src.dir = extd.dir, parm = "y_extinct_coef")
-## To pass them to optim_apsim, combine them
-pp <- c(rue.pth, ext.pth)
-pp
-
-
-
-# get default value sim results -------------------------------------------
-
-sim0raw <- apsim("ames-optim.apsim", src.dir = ".", value = "report")
-
-sim0 <- 
-  sim0raw %>% 
-  mutate(Date = ymd(Date))
-  
-obs_ames <- 
-  read_csv("ames-obs.csv")
-         
-ggplot() + 
-  geom_point(data = obs_ames, aes(x = Date, y = corn_buac)) + 
-  geom_line(data = sim0, aes(x = Date, y = corn_buac)) + 
-  ggtitle("Corn Yield")
-
 
 # find the paths to params ------------------------------------------------
 
-#--let's try doing coldTT. It's a single value, so should be easier than the other strings
+## Want to do coldTT as a trial. It's a single value, so should be easier than the other strings
 
 #--find path to parm I want to optimize
 
+# simulation
 a_xml <- read_xml("ames-optim.apsim")
+
+# path to it
 a_xml %>%
     xml_find_all(., ".//manager2") %>%
     xml_find_all(., paste0(".//", "coldTT")) %>% 
@@ -72,6 +36,10 @@ inspect_apsim("ames-optim.apsim", src.dir = ".",
               node = "Other",
               parm = "coldTT")
 
+inspect_apsim("ames-optim.apsim", src.dir = ".",
+              node = "Other",
+              parm = "manager2")
+
 #--example for finding paths
 ## Finding RUE
 inspect_apsimx_replacement("Wheat-opt-ex.apsimx", src.dir = extd.dir,
@@ -80,13 +48,6 @@ inspect_apsimx_replacement("Wheat-opt-ex.apsimx", src.dir = extd.dir,
                            node.subchild = "Photosynthesis",
                            node.subsubchild = "RUE", 
                            parm = "FixedValue",
-                           verbose = FALSE)
-## Finding BasePhyllochron
-inspect_apsimx_replacement("Wheat-opt-ex.apsimx", src.dir = extd.dir,
-                           node = "Wheat", 
-                           node.child = "Cultivars",
-                           node.subchild = "USA",
-                           node.subsubchild = "Yecora", 
                            verbose = FALSE)
 ## Constructing the paths is straight-forward
 pp1 <- "Wheat.Leaf.Photosynthesis.RUE.FixedValue"
